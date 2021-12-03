c00aa66cf63662c25f0d0f07bb9c9b64a84c35ce
dhrubosabbir <heartedsabbirdhrubo@gmail.com>
2021-12-04 04:37:03 +0600

    update

--- data.js
+++ data.js
@@ -62,5 +62,14 @@ const studentData = [
         S   : 77,
         SS   : 77,
         Reli   : 77,
+      }, {
+        Name : 'Zonayed',
+        Roll : 04,
+        Bn   : 57,
+        En   : 77,
+        Math   : 77,
+        S   : 77,
+        SS   : 77,
+        Reli   : 77,
       },
 ]
