aba0b7992236aa42f2b16f14fc81bfe7e071f5ca
dhrubosabbir <heartedsabbirdhrubo@gmail.com>
2021-12-04 04:29:52 +0600

    update

---
+++ data.js
@@ -0,0 +1,12 @@
+const studentData = [
+    {
+      Name : 'Sabbir',
+      Roll : 04,
+      Bn   : 57,
+      En   : 77,
+      Math   : 77,
+      S   : 77,
+      SS   : 77,
+      Reli   : 77,
+    }
+]
